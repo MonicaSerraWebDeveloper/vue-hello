376cce5502ff5200b048fee6dff11df400480ec1
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-26 14:47:06 +0100

    feat: add the image using the v-bind abbreviation

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: "Secondo il Time la persona dell'anno è: Taylor Swift"
+            message: "Secondo il Time la persona dell'anno è: Taylor Swift",
+            image: "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/KUQN5M3L6FSH5HQLEOX4UO7DSU.JPG&w=1200"
         };
     }
 }).mount('#app');
