ced90ed6a3c1a3d7cf262c7a63b8244757b4742f
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-26 14:46:12 +0100

    feat: add the message with vue using data and add the variable inside the tag h1 using double branketts

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            message: "Secondo il Time la persona dell'anno è: Taylor Swift"
         };
     }
 }).mount('#app');
